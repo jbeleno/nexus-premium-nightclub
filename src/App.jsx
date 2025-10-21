fafb19412966bf43323b50fdc1c3498b8d45c95a
jbeleno <u20212201817@usco.edu.co>
2025-10-21 13:45:09 -0500

    Remove EventModal component and related functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 
 // Componentes
-import EventModal from './components/EventModal'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 
@@ -14,21 +13,6 @@ import Reservas from './pages/Reservas'
 import Eventos from './pages/Eventos'
 
 function App() {
-  const [showEventModal, setShowEventModal] = useState(false)
-
-  useEffect(() => {
-    // Mostrar el modal de evento solo la primera vez que se carga la página
-    const hasSeenModal = localStorage.getItem('hasSeenEventModal')
-    if (!hasSeenModal) {
-      setShowEventModal(true)
-    }
-  }, [])
-
-  const handleCloseModal = () => {
-    setShowEventModal(false)
-    localStorage.setItem('hasSeenEventModal', 'true')
-  }
-
   return (
     <div className="App">
       <Navigation />
@@ -45,13 +29,6 @@ function App() {
       </main>
 
       <Footer />
-
-      {/* Modal de Evento Semanal */}
-      <AnimatePresence>
-        {showEventModal && (
-          <EventModal onClose={handleCloseModal} />
-        )}
-      </AnimatePresence>
     </div>
   )
 }
